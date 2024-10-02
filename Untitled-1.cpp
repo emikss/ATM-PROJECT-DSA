d01f6971b08856c777a0f5528e56b766032e090c
Ralf Galopo Neis <150760749+RalfNeis@users.noreply.github.com>
2024-10-02 15:40:01 +0800

    login, create, file handling
    naka base ako sa mga previous maching prob kaya di ko sure kung gagana here

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,39 +1,137 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
 using namespace std;
 
 void mainMenu();
 
-struct Account{
-    string name;
-    int id;
-    int pass;
+struct LoginResult{       //storing login results
+    bool success;
+    string cardNumber;
 };
 
-class dikopaalam{
+class UserAccount{  //user info and storing it to file handling keme
     private:
-
+        string name;
+        string pin;
+        string cardNumber;
     public:
-    
+        UserAccount(string _name, string _pin){   //constructor kuno (sumunod lang ako tutorial bai)
+            name= _name;
+            pin= _pin;
+            cardNumber=generateCardNum();  //generating random card num
+        }
+
+        string generateCardNum(){   //generating random 10-digit num
+            string cardNum="";
+            srand(time(0));
+            for(int i=0;i<10;i++){
+                cardNum+=to_string(rand()%10);
+            }return cardNum;
+        }
+
+        //gayahin ko lang kay ku
+        void saveToFile(){     //save user info
+            ofstream file("accounts.txt",ios::app);
+            if(file.is_open()){
+                file<<name<<" "<<pin<<" "<< cardNumber;
+                file.close();
+            }else{
+                cout<<"Unable to open file.";
+            }
+        }
+        
+        void displayInfo(){
+            cout<<"Account created successfully!\n";
+            cout<<"Your card number is: "<< cardNumber;
+        }
+
+        static LoginResult login(string _name, string _pin){
+            ifstream file("accounts.txt");
+            string fileName, filePin, fileCardNumber;
+            LoginResult result={false,""}; //initialize daw
+
+            if(file.is_open()){
+                while(file>>fileName>>filePin>>fileCardNumber){
+                    if(fileName== _name && filePin== _pin){
+                        result.success=true;
+                        result.cardNumber=fileCardNumber;
+                        break;
+                    }
+                }
+            }else{
+                cout<<"Unable to open file.";
+            }return result;
+        }
 };
 
+void createAccount(){
+    string name, pin;
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Enter a 4-digit pin: ";
+    cin>>pin;
+
+    UserAccount newUser(name,pin);
+    newUser.saveToFile();
+    newUser.displayInfo();
+}
+
+void userLogin(){
+    string name, pin;
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Enter your pin: ";
+    cin>>pin;
+
+    LoginResult loginResult = UserAccount::login(name, pin);
+
+    if(loginResult.success){
+        cout<<"Login successfull!\n";
+        cout<<"Your card number is: "<<loginResult.cardNumber;
+    }else{
+        cout<<"Invalid name or pin.";
+    }
+}
 
 int main(){
 
     int choice;
 
-    mainMenu();
-  
+    while(1){
+        mainMenu();
+        cin>>choice;
+        switch(choice){
+            case 1:
+            system("cls");
+            userLogin();
+                break;
+            case 2:
+            system("cls");
+            createAccount();
+                break;
+            case 3:
+            system("cls");
+            cout<<"Thank you for using this ATM";
+            default:
+                cout<<"Invalid choice, Please try again.";
+        }
+    } 
 }
 
 void mainMenu(){
-    cout << "***************************";
-    cout << "\n\tATM MACHINE\n";
-    cout << "***************************";
-    cout << "\n1.) Login";
-    cout << "\n2.) Create Account";
-    cout << "\nEnter your choice: ";
+    cout<<"\n***************************";
+    cout<<"\n\tATM MACHINE\n";
+    cout<<"***************************";
+    cout<<"\n1.) Login";
+    cout<<"\n2.) Create Account";
+    cout<<"\n3.) Exit";
+    cout<<"\nEnter your choice: ";
 }
+
 
 
 
