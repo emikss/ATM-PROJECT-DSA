a9536a1926dbc9f1246c077b92070e0c46a426b6
Sean Lucino <sean.lucino1@gmail.com>
2024-10-04 08:31:25 +0800

    POINTER ATM

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -4,16 +4,16 @@
 #include <cstdlib>
 #include <ctime>
 #include <iomanip>
-
 using namespace std;
 
 void mainMenu();
 
-struct accounts{
-    string acc_num;
-    string pin_code;
-    accounts *next;
+struct link{
+    string accNum;
+    string pincode;
+    link *next;
 };
+
 struct LoginResult{       //storing login results
     bool success;
     string cardNumber;
@@ -24,139 +24,130 @@ class UserAccount{  //user info and storing it to file handling keme
         string name;
         string pin;
         string cardNumber;
+
+        link *accountsLink;
     public:
-        
-        UserAccount(string _name, string _pin){   //constructor kuno (sumunod lang ako tutorial bai)
-            name = _name;
-            pin = _pin;
-            // cardNumber = generateCardNum();  //generating random card num
+        UserAccount() : accountsLink(NULL){}; // constructor
+
+        bool isEmpty(){
+            return (accountsLink == NULL);
         }
 
 
-    /*    string generateCardNum(){   //generating random 16-digit num
-            string cardNum = "";
-            srand(time(0)); 
-            for (int i = 0; i < 16 ; i++){
-                cardNum += to_string (rand()%10);
-            } return cardNum;
-        }*/
+        string generateCardNum(){   //generating random 10-digit num
+            string cardNum="";
+            srand(time(0));
+            for(int i=0;i<10;i++){
+                cardNum+=to_string(rand()%10);
+            }return cardNum;
+        }
 
         //gayahin ko lang kay ku
         void saveToFile(){     //save user info
-            ofstream file("accounts.txt", ios::app);
-            if (file.is_open()){
-                file << name << endl << pin << endl << cardNumber << endl;
+            ofstream file("accounts.txt",ios::app);
+            if(file.is_open()){
+                file<<name<<" "<<pin<<" "<< cardNumber;
                 file.close();
             }else{
-                cout << "Unable to open file.";
+                cout<<"Unable to open file.";
             }
         }
 
-        void retrieveToFile(){     //save user info
-        string account_number; string pin;
-            ifstream file("accounts.txt", ios::app);
-            if (file.is_open()){
-                while (!file.eof()){
-                    file >> account_number;
-                    file >> pin;
-                    link(account_number, pin);
-                };
-            } else{
-                cout << "Unable to open file.";
+        void displayInfo(){
+            cout<<"Account created successfully!\n";
+            cout<<"Your card number is: "<< cardNumber;
+        }
+
+        void filetoLink(string fileName, string filePin, string fileCardNumber){ // pushes data of file to a linked list
+            link *newLink;
+            newLink = new link();
+            newLink->accNum = fileCardNumber;
+            newLink->pincode = filePin;
+
+            if (isEmpty()){
+                accountsLink = newLink;
+            } else {
+                accountsLink->next = newLink;
+                accountsLink = newLink;
             }
-            file.close();
-}
+        }
 
-        
-      /* void displayInfo(){
-            system("cls");
-            cout << "Account created successfully!\n";
-            cout << "Your card number is: " << cardNumber;
-        }*/
-       void link(string account_number, string pin){
-        accounts *newNode = new accounts();
-        newNode->acc_num=account_number;
-        newNode->pin_code=pin;
-        newNode=newNode->next;
-       }
-        //static LoginResult login(string _name, string _pin){
-            /*ifstream file ("accounts.txt");
+        void retrieve (){ // TO BE MODIFIED, DAPAT PATI YUNG AMOUNT NG CASH NG ATM KASAMA SA MASASAVE
+            ifstream file("accounts.txt");
             string fileName, filePin, fileCardNumber;
-            LoginResult result = {false,""};        //initialize daw
 
-            if (file.is_open()){
-                while (file>>fileName>>filePin>>fileCardNumber){
-                    if (fileName == _name && filePin == _pin){
-                        result.success = true;
-                        result.cardNumber = fileCardNumber;
-                        break;
-                    }
+            if(file.is_open()){
+                while(file>>fileName>>filePin>>fileCardNumber){
+                    filetoLink(fileName, filePin, fileCardNumber);
                 }
             }else{
-                cout << "Unable to open file.";
-            }return result;*/ 
-        //}
+                cout<<"Unable to open file.";
+            } 
+        }
+
+
+        void userLogin(){
+            string acc_num, pin;
+            cout<<"Enter your account number: ";
+            cin>>acc_num;
+            cout<<"Enter your pin: ";
+            cin>>pin;
+            
+            retrieve();
+
+            if(LoginResult(acc_num, pin) == 1){
+                    cout<<"Login successfull!\n";
+                } else {
+                    cout<<"Invalid name or pin.";
+                }
+        }
+
+        int LoginResult(string acc_num, string pin){
+            link *p = accountsLink;
+
+            while (p!=NULL){
+                if (p->accNum == acc_num){
+                    if(p->pincode == pin){
+                        return 1;
+                    }
+                    return 0;
+                } 
+                p = p->next;
+            }
+            return 0;
+        }
+
 };
-/*void createAccount(){
-    string name, pin;
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "Enter a 4-digit pin: ";
-    cin >> pin;
-
-    UserAccount newUser(name,pin);
-    newUser.saveToFile();
-    newUser.displayInfo();
-}*/
-
-
-void userLogin(){
-    string name, pin;
-    cout << "Enter your number: ";
-    cin >> name;
-    cout << "Enter your pin: ";
-    cin >> pin;
-
-    /*LoginResult loginResult = UserAccount::login(name, pin);
-
-    if(loginResult.success){
-        cout << "Login successfull!\n";
-        cout << "Your card number is: " << loginResult.cardNumber;
-    }else{
-        cout << "Invalid name or pin.";
-    }*/
-}
 
 int main(){
-
+    UserAccount login;
     int choice;
 
     while(1){
         mainMenu();
-        cin >> choice;
+        cin>>choice;
         switch(choice){
             case 1:
             system("cls");
-            userLogin();
+            login.userLogin();
                 break;
-            case 2:
+            case 0:
             system("cls");
-            cout << "Thank you for using this ATM";
+            cout<<"Thank you for using this ATM";
             default:
-                cout << "Invalid choice, Please try again.";
+                cout<<"Invalid choice, Please try again.";
         }
     } 
 }
 
 void mainMenu(){
-    cout << "\n***************************";
-    cout << "\n\tATM MACHINE\n";
-    cout << "***************************";
-    cout << "\n1.) Login";
-    cout << "\n2.) Exit";
-    cout << "\nEnter your choice: ";
+    cout<<"\n***************************";
+    cout<<"\n\tATM MACHINE\n";
+    cout<<"***************************";
+    cout<<"\n1.) Login";
+    cout<<"\n0.) Exit";
+    cout<<"\nEnter your choice: ";
 }
-
 
 
 
